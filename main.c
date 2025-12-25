121ab38ee42b5f09fd6eafc558314f0ba2236801
amirhosin85 <amhs5831@gmail.com>
2025-12-25 12:23:12 -0800

    UPDATED main.c: the sky and ground and floors added.

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include "raylib.h"
 #include "map.h"
 #include "player.h"
+#include "raycaster.h"
 
 int main(void)
 {
-    const int screenWidth = MAP_WIDITH * TILE_SIZE;
-    const int screenHeigh = MAP_HEIGHT * TILE_SIZE;
+    const int screenWidth = 800;
+    //MAP_WIDITH * TILE_SIZE;
+    const int screenHeigh = 600;
+    //MAP_HEIGHT * TILE_SIZE;
 
-    InitWindow(screenWidth, screenHeigh,"Raycasting Engine: phase_1");
+    InitWindow(screenWidth, screenHeigh,"Raycasting Engine: phase_2 (the 3D mode)");
     SetTargetFPS(60);
 
     InitPlayer();
@@ -19,8 +22,14 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(BLACK);
-            DrawMap2D();
-            DrawPlayer2D();
+
+            DrawRectangle(0, 0, screenWidth, screenHeigh / 2, DARKGRAY);
+            DrawRectangle(0,screenHeigh / 2, screenWidth, screenHeigh / 2, BLUE);
+            DrawRaycasting();
+
+            DrawFPS(10,10);
+            //DrawMap2D();
+            //DrawPlayer2D();
             
         EndDrawing();
     }
